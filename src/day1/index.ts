3e3a603e16dbc5a901eb203a9ff34984f85f213c
Gergely Garai <garai.gergely22@gmail.com>
2025-12-01 23:41:49 +0100

    day1 gold star

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -6,8 +6,14 @@ const lines = await readFile(`./src/day1/${file}`, {
 
 const inputs = lines.split("\n");
 
+let extraZeros = 0;
+
 const rotations = inputs.map((r, i) => {
   const valueStr = r.slice(1);
+  if (valueStr.length > 2) {
+    extraZeros += parseInt(valueStr[0]);
+  }
+
   return {
     index: i + 1,
     direction: r[0],
@@ -19,8 +25,6 @@ const rotations = inputs.map((r, i) => {
 });
 
 type Rotation = (typeof rotations)[number];
-
-// console.log(rotations);
 
 const startPosition = 50;
 
@@ -34,30 +38,35 @@ function dial(start: number): number {
 
   const rotation = rotations[index];
 
-  const nextPosition = calculateNextPosition(start, rotation);
+  const [nextPosition, shouldAddExtra] = calculateNextPosition(start, rotation);
   // console.log(
   //   `start postition was ${start}, we rotate ${
   //     rotation.direction + rotation.value
-  //   } NEXT position is ${nextPosition}`
+  //   } NEXT position is ${nextPosition} and shouldAddExtra is ${shouldAddExtra}`
   // );
   index++;
 
   if (nextPosition === 0) {
     zeroCount++;
+  } else if (shouldAddExtra && start !== 0) {
+    extraZeros++;
   }
 
   return dial(nextPosition);
 }
 
-function calculateNextPosition(start: number, rotation: Rotation): number {
+function calculateNextPosition(
+  start: number,
+  rotation: Rotation
+): [number, boolean] {
   if (rotation.direction === "R") {
     const value = start + rotation.value;
-    return value >= 100 ? Math.abs(100 - value) : value;
+    return value >= 100 ? [Math.abs(100 - value), true] : [value, false];
   }
   if (rotation.direction === "L") {
     const value = start - rotation.value;
 
-    return value < 0 ? 100 - Math.abs(value) : value;
+    return value < 0 ? [100 - Math.abs(value), true] : [value, false];
   }
 
   throw Error("bad input");
@@ -65,4 +74,4 @@ function calculateNextPosition(start: number, rotation: Rotation): number {
 
 dial(startPosition);
 
-console.log(zeroCount);
+console.log(zeroCount + extraZeros);
