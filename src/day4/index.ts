046288e6137f1d2d414f5ec59155bfacbb8c15a5
Gergely Garai <garai.gergely22@gmail.com>
2025-12-04 21:52:58 +0100

    day4 silver star

---
+++ src/day4/index.ts
@@ -0,0 +1,58 @@
+import { readFile } from "fs/promises";
+const file = "input.txt";
+const lines = await readFile(`./src/day4/${file}`, {
+  encoding: "utf8",
+});
+
+const rows = lines.split("\n");
+
+type Point = {
+  row: number;
+  column: number;
+  value: string;
+};
+
+const grid: Point[][] = rows.map((row, i) =>
+  Array.from(row).map((point, j) => ({
+    row: i,
+    column: j,
+    value: point,
+  }))
+);
+
+const numberOfColumns = grid[0].length;
+
+function getNeighbors(point: Point, g: Point[][]) {
+  const neighbors: Array<Point> = [];
+  for (let index = 0; index < 3; index++) {
+    const currentRowIndex = point.row + index - 1;
+    if (currentRowIndex < 0 || currentRowIndex >= g.length) {
+      continue;
+    }
+    const columnStart = point.column - 1 < 0 ? 0 : point.column - 1;
+    const columnEnd =
+      point.column + 1 >= numberOfColumns ? numberOfColumns : point.column + 1;
+    const currentRow = g[currentRowIndex]
+      .slice(columnStart, columnEnd + 1)
+      .filter((p) => !(p.column === point.column && p.row === point.row));
+    neighbors.push(...currentRow);
+  }
+  return neighbors;
+}
+
+function solve() {
+  let res = 0;
+  grid.forEach((row) => {
+    row.forEach((point) => {
+      if (point.value !== "@") {
+      } else if (
+        getNeighbors(point, grid).filter((p) => p.value === "@").length < 4
+      ) {
+        res++;
+      }
+    });
+  });
+  return res;
+}
+
+console.log(solve());
